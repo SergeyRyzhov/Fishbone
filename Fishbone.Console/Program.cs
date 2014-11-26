4b1e71cc09ec40c3fcc8987678f07d87ec48dd45
SergeyRyzhov <serega.rf@gmail.com>
2014-11-26 21:06:08 +0300

    Initial commit

---
+++ Fishbone.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Window = System.Console;
+
+namespace Fishbone.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Window.WriteLine("Invalid arguments");
+                return;
+            }
+
+            Window.WriteLine("Start parsing arguments");
+
+        }
+    }
+
+    interface IParser
+    {
+         
+    }
+
+    class MtxParser : IParser
+    {
+        
+    }
+}
